61bb29202d775cb0e99bf19d331e0c3c543b3161
Tobias Sijtsma <tobias@tobiass.nl>
2017-05-25 02:14:49 +0200

    first version

---
+++ js/app.js
@@ -0,0 +1,439 @@
+/**
+ * Written by Tobias Sijtsma
+ * GitHub @Tobiaqs
+ */
+"use strict";
+
+(function () {
+	window.cTMEmulator = window.cTMEmulator || {};
+
+	if (!window.localStorage) {
+		alert("No localStorage support");
+		return;
+	}
+
+	if (!window.localStorage.cTMEmulatorFolder) {
+		window.localStorage.cTMEmulatorFolder = "{}";
+	}
+
+	// Allows for "file" storage in localStorage
+	let files = {
+		save: (filename, content) => {
+			let folder = JSON.parse(window.localStorage.cTMEmulatorFolder);
+			folder[filename] = content;
+			window.localStorage.cTMEmulatorFolder = JSON.stringify(folder);
+		},
+		read: (filename) => {
+			let folder = JSON.parse(window.localStorage.cTMEmulatorFolder);
+			return folder[filename];
+		},
+		delete: (filename) => {
+			let folder = JSON.parse(window.localStorage.cTMEmulatorFolder);
+			delete folder[filename];
+			window.localStorage.cTMEmulatorFolder = JSON.stringify(folder);
+		},
+		exists: (filename) => {
+			let folder = JSON.parse(window.localStorage.cTMEmulatorFolder);
+			return !!folder[filename];
+		},
+		list: () => {
+			let folder = JSON.parse(window.localStorage.cTMEmulatorFolder);
+			return Object.keys(folder);
+		}
+	};
+
+	// Declare state variables
+	let currentFilename;
+	let activeWrapperName;
+
+	// Declare DOM element holders
+	let editor;
+	let browse;
+	let toolbarBtnBack;
+	let toolbarBtnNew;
+	let toolbarBtnBrowse;
+	let toolbarBtnSave;
+	let toolbarBtnEmulate;
+	let toolbarBtnTest;
+	let toolbarBtnValidate;
+
+	// The cTM instance used by the emulator
+	let emulatorCTM = new cTMEmulator.cTM;
+	let emulatorSteps;
+	let emulatorInput;
+	let emulatorError;
+	let emulatorStats;
+	let emulatorTape;
+	let emulatorBtnStep;
+	let emulatorBtnRun;
+	let emulatorBtnRestart;
+
+	let wrappers = {
+		browse: {
+			onEnter: () => {
+				// Create links
+				let links = [];
+				files.list().forEach((file) => {
+					links.push("<a href=\"javascript:void(0);\" data-open-file=\"" + file + "\">Load " + file + "</a> (<a href=\"javascript:void(0);\" data-delete-file=\"" + file + "\">delete</a>)");
+				});
+				browse.innerHTML = links.join("<br>");
+
+				if (browse.innerHTML === "") {
+					browse.innerHTML = "No files were found!";
+				}
+
+				// Add listeners to the links
+				[].forEach.call(browse.getElementsByTagName("A"), (node) => {
+					if (node.attributes["data-open-file"]) {
+						node.addEventListener("click", () => {
+							loadProgram(node.attributes["data-open-file"].value);
+						});
+					} else if (node.attributes["data-delete-file"]) {
+						node.addEventListener("click", () => {
+							removeProgram(node.attributes["data-delete-file"].value);
+						});
+					}
+				});
+			},
+			onLeave: () => {
+				browse.innerHTML = "";
+			}
+		},
+
+		emulator: {
+			onEnter: () => {
+				wrappers.emulator.onLeave();
+			},
+			onLeave: (leaveInput) => {
+				emulatorCTM.reset();
+
+				if (!leaveInput) {
+					emulatorInput.value = "";
+				}
+
+				emulatorBtnStep.disabled = true;
+				emulatorBtnRun.disabled = true;
+				emulatorError.innerHTML = "";
+				emulatorTape.innerHTML = "";
+				emulatorStats.innerHTML = "";
+				emulatorSteps = 0;
+			}
+		}
+	};
+
+	window.addEventListener("load", () => {
+		// Get elements from DOM
+		editor = document.getElementsByClassName("editor")[0];
+		browse = document.getElementsByClassName("browse")[0];
+		toolbarBtnBack = document.getElementsByClassName("toolbar-btn-back")[0];
+		toolbarBtnNew = document.getElementsByClassName("toolbar-btn-new")[0];
+		toolbarBtnBrowse = document.getElementsByClassName("toolbar-btn-browse")[0];
+		toolbarBtnSave = document.getElementsByClassName("toolbar-btn-save")[0];
+		toolbarBtnEmulate = document.getElementsByClassName("toolbar-btn-emulate")[0];
+		toolbarBtnTest = document.getElementsByClassName("toolbar-btn-test")[0];
+		toolbarBtnValidate = document.getElementsByClassName("toolbar-btn-validate")[0];
+
+		emulatorInput = document.getElementsByClassName("emulator-input")[0];
+		emulatorError = document.getElementsByClassName("emulator-error")[0];
+		emulatorStats = document.getElementsByClassName("emulator-stats")[0];
+		emulatorTape = document.getElementsByClassName("emulator-tape")[0];
+		emulatorBtnStep = document.getElementsByClassName("emulator-btn-step")[0];
+		emulatorBtnRun = document.getElementsByClassName("emulator-btn-run")[0];
+		emulatorBtnRestart = document.getElementsByClassName("emulator-btn-restart")[0];
+
+		// Bind listeners
+		toolbarBtnNew.addEventListener("click", newProgram);
+
+		toolbarBtnBrowse.addEventListener("click", () => {
+			setActiveWrapper("browse");
+		});
+
+		toolbarBtnSave.addEventListener("click", () => {
+			// Don't allow clicks when the text is equal to Saved!
+			if (toolbarBtnSave.innerHTML === "Saved!") {
+				return;
+			}
+
+			if (currentFilename) {
+				files.save(currentFilename, editor.value);
+				toolbarBtnSave.innerHTML = "Saved!";
+				setTimeout(() => {
+					toolbarBtnSave.innerHTML = "Save";
+				}, 1000);
+			} else {
+				saveProgramAs();
+			}
+		});
+
+		toolbarBtnEmulate.addEventListener("click", () => {
+			setActiveWrapper("emulator");
+		});
+
+		toolbarBtnBack.addEventListener("click", () => {
+			setActiveWrapper("editor");
+		});
+
+		emulatorInput.addEventListener("keyup", onEmulatorInputChanged);
+
+		emulatorBtnStep.addEventListener("click", () => {
+			// If no error, add to the step counter
+			if (emulatorCTM.step()) {
+				emulatorSteps ++;
+			}
+
+			if (emulatorCTM.isFinished()) {
+				emulatorBtnStep.disabled = true;
+				emulatorBtnRun.disabled = true;
+			}
+
+			updateTapeRepresentation();
+			updateStats();
+		});
+
+		emulatorBtnRun.addEventListener("click", () => {
+			// Run until finished
+			while (!emulatorCTM.isFinished()) {
+				// Or: until an error occurs
+				if (!emulatorCTM.step()) {
+					break;
+				}
+				emulatorSteps ++;
+			}
+
+			if (emulatorCTM.isFinished()) {
+				emulatorBtnStep.disabled = true;
+				emulatorBtnRun.disabled = true;
+			}
+
+			updateTapeRepresentation();
+			updateStats();
+		});
+
+		emulatorBtnRestart.addEventListener("click", () => {
+			onEmulatorInputChanged();
+		});
+
+		emulatorCTM.on("error", (error) => {
+			emulatorError.innerHTML = error;
+		});
+
+		setActiveWrapper("editor");
+	});
+
+	// Functions
+
+	// Simple handler for the emulator
+	function onEmulatorInputChanged () {
+		// Reset everything but keep input
+		wrappers.emulator.onLeave(true);
+		emulatorCTM.initialize(editor.value, emulatorInput.value.split(""));
+
+		emulatorBtnStep.disabled = false;
+		emulatorBtnRun.disabled = false;
+
+		updateTapeRepresentation();
+		updateStats();
+	};
+
+	// Show a popup asking for a filename, then set currentFilename and click save button. 
+	function saveProgramAs () {
+		swal({
+			title: "Enter a name",
+			text: "Please give your program a name:",
+			type: "input",
+			showCancelButton: true,
+			closeOnConfirm: false,
+			animation: "slide-from-top",
+			inputPlaceholder: "No extension required!"
+		}, (input) => {
+			if (input === false) {
+				return false;
+			}
+
+			if (input === "") {
+				swal.showInputError("Please enter a name.");
+				return false;
+			}
+
+			if (!/^[A-Za-z0-9\-_ \.\,]+$/.test(input)) {
+				swal.showInputError("This name is not allowed.");
+				return false;
+			}
+
+			if (files.exists(input)) {
+				swal.showInputError("A program with this name already exists.");
+				return false;
+			}
+
+			currentFilename = input;
+
+			swal.close();
+
+			toolbarBtnSave.click();
+		});
+	};
+
+	// New button clicked.
+	function newProgram () {
+		// Two cases in which creating a new file is allowed without confirmation
+		if (isProgramPristine()) {
+			currentFilename = null;
+			editor.value = "";
+			return;
+		}
+
+		// Otherwise, ask the user if they are sure
+		swal({
+			title: "Are you sure?",
+			text: "Your unsaved changes will be lost.",
+			type: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#DD6B55",
+			confirmButtonText: "Yes, new file!",
+			closeOnConfirm: false
+		}, () => {
+			currentFilename = null;
+			editor.value = "";
+			swal.close();
+		});
+	};
+
+	// From browse: load a program
+	function loadProgram (filename) {
+		// Two cases in which loading a file is allowed without confirmation
+		if (isProgramPristine()) {
+			currentFilename = filename;
+			editor.value = files.read(filename);
+			setActiveWrapper("editor");
+			return;
+		}
+
+		// Otherwise, ask the user if they are sure
+		swal({
+			title: "Are you sure?",
+			text: "Your unsaved changes will be lost.",
+			type: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#DD6B55",
+			confirmButtonText: "Yes, load file!",
+			closeOnConfirm: false
+		}, () => {
+			currentFilename = filename;
+			editor.value = files.read(filename);
+			setActiveWrapper("editor");
+			swal.close();
+		});
+	};
+
+	// From browse: remove a program
+	function removeProgram (filename) {
+		// Ask the user if they are sure
+		swal({
+			title: "Are you sure?",
+			text: "This file will be deleted.",
+			type: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#DD6B55",
+			confirmButtonText: "Yes, delete file!",
+			closeOnConfirm: false
+		}, () => {
+			// Delete the file
+			files.delete(filename);
+
+			// If the deleted file was loaded, clear out editor
+			if (filename === currentFilename) {
+				editor.value = "";
+				currentFilename = null;
+			}
+
+			swal.close();
+
+			// Update list
+			wrappers.browse.onEnter();
+		});
+	};
+
+	// Is current file saved or does not need to be saved
+	function isProgramPristine () {
+		return (currentFilename && files.exists(currentFilename) && files.read(currentFilename) === editor.value) || (!currentFilename && editor.value.length === 0);
+	}
+
+	function setActiveWrapper (wrapperName) {
+		// Don't do anything if not required
+		if (wrapperName === activeWrapperName) {
+			return;
+		}
+
+		// Run onLeave listener
+		if (activeWrapperName && wrappers[activeWrapperName]) {
+			wrappers[activeWrapperName].onLeave();
+		}
+
+		let wrapperNodes = document.getElementsByClassName("wrapper");
+		[].forEach.call(wrapperNodes, (wrapperNode) => {
+			if (wrapperNode.classList.contains("wrapper-" + wrapperName)) {
+				wrapperNode.classList.add("visible");
+			} else {
+				wrapperNode.classList.remove("visible");
+			}
+		});
+
+		activeWrapperName = wrapperName;
+
+		if (wrapperName !== "editor") {
+			// Show back, hide file manipulation buttons
+			toolbarBtnBack.classList.remove("hidden");
+			toolbarBtnNew.classList.add("hidden");
+			toolbarBtnBrowse.classList.add("hidden");
+			toolbarBtnSave.classList.add("hidden");
+		} else {
+			// Hide back, show file manipulation buttons
+			toolbarBtnBack.classList.add("hidden");
+			toolbarBtnNew.classList.remove("hidden");
+			toolbarBtnBrowse.classList.remove("hidden");
+			toolbarBtnSave.classList.remove("hidden");
+		}
+
+		// Run onEnter listener
+		if (wrappers[wrapperName]) {
+			wrappers[wrapperName].onEnter();
+		}
+	};
+
+	// In emulator: update the representation of the tape
+	function updateTapeRepresentation () {
+		let data = emulatorCTM.tape.getData();
+		let position = emulatorCTM.tape.getPosition();
+		let blank = "<div class=\"emulator-tape-element\">#</div>";
+		let blankSelected = "<div class=\"emulator-tape-element selected\">#</div>";
+		let centerHTML = "";
+
+		data.forEach((element, index) => {
+			centerHTML += "<div class=\"emulator-tape-element" + (index === position ? " selected" : "") + "\">" + element + "</div>";
+		});
+
+		if (position < 0) {
+			emulatorTape.innerHTML = blankSelected;
+			for (let i = 0; i < -position - 1; i ++) {
+				emulatorTape.innerHTML += blank;
+			}
+			emulatorTape.innerHTML += centerHTML + blank;
+		} else if (position >= 0 && position < data.length) {
+			emulatorTape.innerHTML = blank + centerHTML + blank;
+		} else if (position >= data.length) {
+			emulatorTape.innerHTML = blank + centerHTML;
+
+			for (let i = 0; i < position - data.length; i ++) {
+				emulatorTape.innerHTML += blank;
+			}
+
+			emulatorTape.innerHTML += blankSelected;
+		}
+	};
+
+	// In emulator: update statistics
+	function updateStats () {
+		emulatorStats.innerHTML = "Steps: " + emulatorSteps + "<br>" +
+			"State: " + emulatorCTM.state;
+	};
+})();
---
+++ js/ctm.js
@@ -0,0 +1,258 @@
+/**
+ * Written by Tobias Sijtsma
+ * GitHub @Tobiaqs
+ */
+"use strict";
+
+(function () {
+	window.cTMEmulator = window.cTMEmulator || {};
+
+	function Tape (data) {
+		// Initial position is set to 0
+		this._position = 0;
+		this._data = data;
+	};
+
+	/**
+	 * #moveRight()
+	 */
+	Tape.prototype.moveLeft = function () {
+		this._position --;
+	};
+
+	/**
+	 * #moveRight()
+	 */
+	Tape.prototype.moveRight = function () {
+		this._position ++;
+	};
+
+	/**
+	 * #read()
+	 * returns: the value on the tape at the current position
+	 */
+	Tape.prototype.read = function () {
+		// Read a value if position is on the tape, otherwise #
+		if (this.isPositionOnTape()) {
+			return this._data[this._position];
+		}
+		return "#";
+	};
+
+	/**
+	 * #write()
+	 * param value: the value to write to the current position on the tape
+	 */
+	Tape.prototype.write = function (value) {
+		// Write if the position is on the tape. Otherwise, extend the tape and
+		// shift position if necessary
+		if (this.isPositionOnTape()) {
+			this._data[this._position] = value;
+		} else if (this._position < 0) {
+			for (let i = 0; i < -this._position; i ++) {
+				this._data.unshift("#");
+			}
+
+			this._position = 0;
+			this._data[this._position] = value;
+		} else if (this._position >= this._data.length) {
+			for (let i = 0; i < this._position - this._data.length + 1; i ++) {
+				this._data.push("#");
+			}
+
+			this._data[this._position] = value;
+		}
+	};
+
+	/**
+	 * #getData()
+	 * returns: the raw underlying array
+	 */
+	Tape.prototype.getData = function () {
+		return this._data;
+	};
+
+	/**
+	 * #getPosition()
+	 * returns: the current position on the tape
+	 */
+	Tape.prototype.getPosition = function () {
+		return this._position;
+	};
+
+	Tape.prototype.isPositionOnTape = function () {
+		return this._position >= 0 && this._position < this._data.length;
+	};
+
+	function cTM () {
+	};
+
+	/**
+	 * #initialize()
+	 * param program: the program in ctm language
+	 * param data: the data in an array. elements should be strings
+	 */
+	cTM.prototype.initialize = function (program, data) {
+		// Initialize transitions array
+		this.transitions = [];
+
+		// States associative array, used to find initial and final states
+		let states = {};
+
+		// Iterate over program lines
+		let errorOccurred = program.split("\n").some((line) => {
+			line = line.trim();
+
+			// If the length or the index of "%%" === 0..
+			if (line.length === 0 || line.indexOf("%%") === 0) {
+				return;
+			}
+
+			// Split the program line 
+			let split = line.split(" ");
+
+			// Convert the program line into a transition object
+			let transition = {
+				fromState: split[0],
+				toState: split[2],
+				input: split[1][0],
+				output: split[1][2],
+				direction: split[1][4]
+			}
+
+			if (transition.direction !== "L" && transition.direction !== "R") {
+				this._emit("error", "Direction " + transition.direction + " was given, L or R expected.");
+				return true;
+			}
+
+			// Add the from state to the state array if it doesn't exist yet
+			if (!states[transition.fromState]) {
+				states[transition.fromState] = { initial: true, final: true };
+			}
+
+			// Add the to state to the state array if it doesn't exist yet
+			if (!states[transition.toState]) {
+				states[transition.toState] = { initial: true, final: true };
+			}
+
+			// If we're not dealing with a transition that doesn't change states..
+			if (transition.fromState !== transition.toState) {
+				// We know that the from state is not final, because it has a transition
+				// to another state
+				states[transition.fromState].final = false;
+
+				// We know the to state is not initial, since it has a transition incoming
+				states[transition.toState].initial = false;
+			}
+
+			// Add transition to the transitions array
+			this.transitions.push(transition);
+		});
+
+		if (errorOccurred) {
+			this.reset();
+			return;
+		}
+
+		// Keep initial and final state names in these arrays
+		let initialStates = [];
+		let finalStates = [];
+
+		for (let state in states) {
+			if (states[state].initial) {
+				initialStates.push(state);
+			}
+			if (states[state].final) {
+				finalStates.push(state);
+			}
+		}
+
+		// More than one initial state?
+		if (initialStates.length !== 1) {
+			this._emit("error", initialStates.length + " initial states found. 1 expected.");
+			this.reset();
+			return;
+		}
+
+		// More than one final state?
+		if (finalStates.length !== 1) {
+			this._emit("error", finalStates.length + " final states found. 1 expected.");
+			this.reset();
+			return;
+		}
+
+		// Set initial and final states
+		this.initialState = initialStates[0];
+		this.finalState = finalStates[0];
+
+		// Set the current state to the initial state
+		this.state = this.initialState;
+
+		// Create a Tape using the data
+		this.tape = new Tape(data);
+
+		// Trigger the initialized event
+		this._emit("initialized");
+	};
+
+	/**
+	 * #step()
+	 * returns: true if no error, false if error
+	 */
+	cTM.prototype.step = function () {
+		let transition = this.transitions.find((transition) => (transition.fromState === this.state && transition.input === this.tape.read()));
+
+		if (transition) {
+			this.tape.write(transition.output);
+
+			if (transition.direction === "L") {
+				this.tape.moveLeft();
+			} else if (transition.direction === "R") {
+				this.tape.moveRight();
+			}
+
+			this.state = transition.toState;
+			return true;
+		} else if (this.state !== this.finalState) {
+			this._emit("error", "No more transitions, but not in final state!");
+			return false;
+		}
+
+		return true;
+	};
+
+	/**
+	 * #isFinished()
+	 * returns: cTM in the final state: true. otherwise false
+	 */
+	cTM.prototype.isFinished = function () {
+		return this.state === this.finalState;
+	};
+
+	/**
+	 * #reset()
+	 */
+	cTM.prototype.reset = function () {
+		delete this.transitions;
+		delete this.initialState;
+		delete this.finalState;
+		delete this.state;
+		delete this.tape;
+	};
+
+	let eventListeners = {
+		error: [],
+		initialized: []
+	};
+
+	cTM.prototype._emit = function (eventName, data) {
+		eventListeners[eventName].forEach((listener) => listener(data));
+	};
+
+	cTM.prototype.on = function (eventName, listener) {
+		eventListeners[eventName].push(listener);
+	};
+
+	window.cTMEmulator.Tape = Tape;
+	window.cTMEmulator.cTM = cTM;
+})();
